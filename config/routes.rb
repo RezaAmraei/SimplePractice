641073688db9b0671c9ebd84c98537ce4db0af05
Reza Amraei <amraeireza@gmail.com>
2022-08-09 22:58:50 -0700

    removed some unused files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :home
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     get 'api/appointments', :to => "appointments#appointments"
     get 'api/appointments/:filter', :to => "appointments#appointments"
